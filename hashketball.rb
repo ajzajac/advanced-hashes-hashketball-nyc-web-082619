e4ed9cc1965c2de724f2ca250475e5ee2f44fb8d
Andrew Zajac <ajzajac@icloud.com>
2019-08-18 17:51:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -155,5 +155,6 @@ def player_numbers(team_name)
  numbers = []
  game_hash.each do |team, team_data|
    if team_data[:team_name] == team_name
-     team_data[:players] 
+     team_data[:players].each do |name, stats|
+       
 end  
