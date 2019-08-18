c9e4a3f7ca3f15cdc6d0636f770aece7d379f464
Andrew Zajac <ajzajac@icloud.com>
2019-08-18 17:40:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -139,7 +139,7 @@ end
 
 def team_colors(team_name)
   game_hash.each do |team, team_data|
-    if team_data[:team_name] == team
+    if team_data[:team_name] == team_name
       return team[:colors]
     end
   end
