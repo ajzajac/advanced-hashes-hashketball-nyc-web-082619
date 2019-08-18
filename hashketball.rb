0ae17927ee9c969b6d320fd6119173d98abf20f4
Andrew Zajac <ajzajac@icloud.com>
2019-08-18 16:51:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -119,7 +119,7 @@ end
 def num_points_scored(name)
   game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
-    data.each do |stat|
+    data[:players].each do |stat|
       
      
     end
